bdbee9cd4cd6741c6734ce2614e6f8406b0fa175
Agrim Sahgal <agrims@synapseco.com>
2024-03-04 07:47:45 +0530

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Purchase Order PDF in Laravel</title>
         <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
     </head>
     <body>
